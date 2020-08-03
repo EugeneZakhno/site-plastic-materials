176e4f2f5dab8168b8f6f0637be19e9dba718ad8
EugeneZakhno <zohan1234567@gmail.com>
2020-08-03 23:31:14 +0300

    first commit

---
+++ src/main/java/io/github/eugenezakhno/siteplasticmaterials/SitePlasticMaterialsApplication.java
@@ -0,0 +1,13 @@
+package io.github.eugenezakhno.siteplasticmaterials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitePlasticMaterialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitePlasticMaterialsApplication.class, args);
+    }
+
+}
